40fe4104b081f71842ce9ee574318957b8060621
Dominik Röschke <dominik.roeschke@web.de>
2022-04-17 23:33:51 +0200

    Finished Armarium Mode Switch

--- src/main/java/de/sarenor/arsinstrumentum/items/curios/armarium/ArmariumStorage.java
+++ src/main/java/de/sarenor/arsinstrumentum/items/curios/armarium/ArmariumStorage.java
@@ -2,9 +2,12 @@ package de.sarenor.arsinstrumentum.items.curios.armarium;
 
 import de.sarenor.arsinstrumentum.ArsInstrumentum;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,8 +18,10 @@ public class ArmariumStorage {
     private static final String CURRENT_SLOT = "current_slot";
     private static final String FLAVORTEXT = "flavortext"; //TODO: Actually write Flavortext
     private static final String IS_HOTBAR_SWITCH = "is_hotbar_switch";
-    private static final String WILL_SWITCH_HOTBAR = "Wizards Armarium will switch your Hotbar Items";
-    private static final String WONT_SWITCH_HOTBAR = "Wizards Armarium won't switch your Hotbar Items";
+    private static final TextComponent WILL_SWITCH_HOTBAR = new TextComponent("Wizards Armarium will switch your Hotbar Items");
+    private static final TextComponent WONT_SWITCH_HOTBAR = new TextComponent("Wizards Armarium won't switch your Hotbar Items");
+    private static final TextComponent HOTBAR_WARNING = new TextComponent("Remove Hotbar Items from Armarium before switching mode!");
+    private static final Style HOTBAR_WARNING_STYLE = Style.EMPTY.withColor(TextColor.fromRgb((int) Long.parseLong("AA0000", 16))).withUnderlined(true);
 
     private final Map<Slots, ArmariumSlot> armariumSlots = new HashMap<>();
     private Slots currentSlot;
@@ -44,6 +49,9 @@ public class ArmariumStorage {
 
     public void switchIsHotbarSwitch() {
         isHotbarSwitch = !isHotbarSwitch;
+        for (ArmariumSlot slot : armariumSlots.values()) {
+            slot.setHotbar(Collections.emptyList());
+        }
         writeArmariumStorageToArmariumItem();
     }
 
@@ -67,11 +75,13 @@ public class ArmariumStorage {
     }
 
     public List<TextComponent> getTooltip() {
+        HOTBAR_WARNING.setStyle(HOTBAR_WARNING_STYLE);
         if (!armariumSlots.isEmpty()) {
-            return List.of(isHotbarSwitch ? new TextComponent(WILL_SWITCH_HOTBAR) : new TextComponent(WONT_SWITCH_HOTBAR),
+            return List.of(HOTBAR_WARNING,
+                    isHotbarSwitch ? WILL_SWITCH_HOTBAR : WONT_SWITCH_HOTBAR,
                     new TextComponent("Next slots armor is: " + armariumSlots.get(Slots.getNextSlot(currentSlot)).listArmor()));
         } else {
-            return List.of(isHotbarSwitch ? new TextComponent(WILL_SWITCH_HOTBAR) : new TextComponent(WONT_SWITCH_HOTBAR));
+            return List.of(HOTBAR_WARNING, isHotbarSwitch ? WILL_SWITCH_HOTBAR : WONT_SWITCH_HOTBAR);
         }
     }
 
